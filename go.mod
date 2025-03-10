21a4a52679ec71f5fe709ff96daf7cb910e4b832
Frenek Lopez <eric.frenek.lr@gmail.com>
2025-03-09 20:30:07 -0700

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/freneklopez/gocurso
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/freneklopez/gocurso/variables"
+
+func main() {
+	variables.MuestroEnteros()
+}
