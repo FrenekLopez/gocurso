eadc2441883cabbce3a21512e939bd53ea607072
Frenek Lopez <eric.frenek.lr@gmail.com>
2025-04-06 10:06:05 -0700

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,35 +1,38 @@
 package main
 
-import "github.com/freneklopez/gocurso/funciones"
+import "github.com/freneklopez/gocurso/arreglos_slices"
 
 func main() {
 	/*estado, texto := variables.ConviertoaTexto(1577)
-	fmt.Println(estado)
-	fmt.Println(texto)*/
+	  fmt.Println(estado)
+	  fmt.Println(texto)*/
 	/*if os := runtime.GOOS; os == "linux" || os == "OS X." {
-		fmt.Println("Esto no es Windows", os)
-	} else {
-		fmt.Println("Esto es Windows")
-	}
+	  	fmt.Println("Esto no es Windows", os)
+	  } else {
+	  	fmt.Println("Esto es Windows")
+	  }
 
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es Linux")
-	case "darwin":
-		fmt.Println("Esto es Darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}*/
+	  switch os := runtime.GOOS; os {
+	  case "linux":
+	  	fmt.Println("Esto es Linux")
+	  case "darwin":
+	  	fmt.Println("Esto es Darwin")
+	  default:
+	  	fmt.Printf("%s \n", os)
+	  }*/
 	/*numero, mensaje := ejercicios.ConvirtiendoaNumero("rfrf")
-	fmt.Println(numero)
-	fmt.Println(mensaje)*/
+	  fmt.Println(numero)
+	  fmt.Println(mensaje)*/
 	/*teclado.IngresoNumeros()*/
 	//fmt.Println(ejercicios.Multiplicacion())
 
-	//files.GrabaTabla()
-	//files.SumaTabla()
-	//files.LeoArchivo()
-	//funciones.Calculo()
-	funciones.LlamarClosure()
-
+	// files.GrabaTabla()
+	// files.SumaTabla()
+	// files.LeoArchivo()
+	// funciones.Calculo()
+	// funciones.LlamarClosure()
+	//funciones.Exponencia(2)
+	//arreglos_slices.MuestroArreglos()
+	//arreglos_slices.MuestroSlices()
+	arreglos_slices.Capacidad()
 }
